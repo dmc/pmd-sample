dd72b110be928be1179af7ffb6f161adce0f4416
Kenichi Fukunaga <dmc.system.service@gmail.com>
2021-11-10 08:04:27 +0000

    remove unused fields

--- src/main/com/service/system/dmc/Problem.java
+++ src/main/com/service/system/dmc/Problem.java
@@ -3,15 +3,6 @@ package com.service.system.dmc;
 public class Problem {
 
     private int number;
-
-    // 利用してない変数として指摘される
-    private String type;
-
-    // 利用してない変数として指摘される
-    private String category;
-
-    // 利用してない変数として指摘される
-    private String comment;
    
     public Problem(int number){
         this.number = number;
