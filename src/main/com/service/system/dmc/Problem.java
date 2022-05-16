c232d6b18224ca4f860e12518a0fedb4fb226ecc
Kenichi Fukunaga <dmc.system.service@gmail.com>
2022-05-16 12:24:11 +0900

    Update Problem.java

--- src/main/com/service/system/dmc/Problem.java
+++ src/main/com/service/system/dmc/Problem.java
@@ -6,12 +6,6 @@ public class Problem {
 
     // 利用してない変数として指摘される
     private String type;
-
-    // 利用してない変数として指摘される
-    private String category;
-
-    // 利用してない変数として指摘される
-    private String comment;
    
     public Problem(int number){
         this.number = number;
