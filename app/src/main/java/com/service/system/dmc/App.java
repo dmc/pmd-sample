64e7d49fe2ebf28473fd03073574f0426fb3827c
dmc <dmc@dmc-air.local>
2022-06-02 16:49:28 +0900

    mod message

--- app/src/main/java/com/service/system/dmc/App.java
+++ app/src/main/java/com/service/system/dmc/App.java
@@ -3,7 +3,7 @@ package com.service.system.dmc;
 public class App {
 
   public static void main(String[] args) {
-    Problem problem = new Problem(1);
-    System.out.println(problem.getNumber());
+    Problem problem = new Problem(1, "world", "category", "hello");
+    System.out.println(problem.getComment() + " " + problem.getCategory());
   }
 }
--- app/src/main/java/com/service/system/dmc/Problem.java
+++ app/src/main/java/com/service/system/dmc/Problem.java
@@ -13,11 +13,20 @@ public class Problem {
   // 利用してない変数として指摘される
   private String comment;
 
-  public Problem(int number) {
+  public Problem(int number, String type, String category, String comment) {
     this.number = number;
+    this.type = type;
+    this.category = category;
+    this.comment = comment;
   }
 
   public int getNumber() {
     return this.number;
+  }
+  public String getComment() {
+    return this.comment;
+  }
+  public String getCategory() {
+    return this.category;
   }
 }
